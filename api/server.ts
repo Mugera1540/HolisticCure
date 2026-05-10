b9ca54de9397c0f2e462b52f4f23bf7e3381c2b5
Mugera1540 <mugera1540@gmail.com>
2026-05-10 21:49:59 +0530

    Fixed 404 Error

---
+++ api/server.ts
@@ -0,0 +1,7 @@
+export default async (request: Request) => {
+  // @ts-ignore - Dynamic import to access the internal server handler
+  const { default: handler } = await import("@tanstack/react-start/server-entry");
+  
+  // The handler is an object with a .fetch() method
+  return handler.fetch(request);
+};
